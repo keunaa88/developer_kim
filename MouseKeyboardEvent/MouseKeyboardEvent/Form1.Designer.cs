9557e18aad6a575f63b11d17255eff505b8f80c5
keunaa88 <keunaa88@gmail.com>
2018-11-13 11:52:26 +0800

    Add files via upload
    First version of Mouse/Keyboard Event

---
+++ MouseKeyboardEvent/MouseKeyboardEvent/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace MouseKeyboardEvent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblCopy = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblBackSpaceCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(288, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lblCopy
+            // 
+            this.lblCopy.AutoSize = true;
+            this.lblCopy.Location = new System.Drawing.Point(30, 98);
+            this.lblCopy.Name = "lblCopy";
+            this.lblCopy.Size = new System.Drawing.Size(94, 13);
+            this.lblCopy.TabIndex = 2;
+            this.lblCopy.Text = "Copy from textbox:";
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(30, 124);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(65, 13);
+            this.lblCount.TabIndex = 3;
+            this.lblCount.Text = "Text Count: ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MouseKeyboardEvent.Properties.Resources.mickey;
+            this.pictureBox1.Location = new System.Drawing.Point(33, 194);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(192, 97);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 308);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(278, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Please click the black tiny cutie nose of Mickey Mouse :) ";
+            // 
+            // lblBackSpaceCount
+            // 
+            this.lblBackSpaceCount.AutoSize = true;
+            this.lblBackSpaceCount.Location = new System.Drawing.Point(30, 152);
+            this.lblBackSpaceCount.Name = "lblBackSpaceCount";
+            this.lblBackSpaceCount.Size = new System.Drawing.Size(100, 13);
+            this.lblBackSpaceCount.TabIndex = 8;
+            this.lblBackSpaceCount.Text = "BackSpace Count: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 365);
+            this.Controls.Add(this.lblBackSpaceCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.lblCopy);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblCopy;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblBackSpaceCount;
+    }
+}
+
---
+++ MouseKeyboardEvent/MouseKeyboardEvent/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseKeyboardEvent
+{
+    public partial class Form1 : Form
+    {
+
+        KeyPressEventHandler keyPressedMethod;
+        int countKeystrokes = 0;
+        int countBackSpace = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+  
+            // define an event handler for this textbox to handle keypresses
+            keyPressedMethod = new KeyPressEventHandler(CustomKeyPress);
+            textBox1.KeyPress += keyPressedMethod;
+        }
+        
+        private void CustomKeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Only duplicate the keystroke onscreen if it is an alphanumeric or numeric character
+            if ((e.KeyChar >= (char)Keys.A && e.KeyChar <= 122) || (e.KeyChar >= (char)Keys.D0 && e.KeyChar <= (char)Keys.D9) )
+            {
+                // 122 is the ascii value for lowercase 'z' char
+                lblCopy.Text += e.KeyChar.ToString();
+            }
+
+            if(e.KeyChar == Convert.ToChar(8))
+            {
+                countBackSpace++;
+                lblBackSpaceCount.Text = "BackSpace Count: "+ countBackSpace;
+            }
+            else
+            {
+                countKeystrokes++;
+                lblCount.Text = "Text Count: " + countKeystrokes.ToString();
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            Point mousePt = new Point(e.X, e.Y);
+            
+
+            if((e.X > 115 && e.X < 130) && (e.Y > 15 && e.Y < 30))
+
+            {
+                MessageBox.Show("Good job!");
+            }
+            else
+            {
+                MessageBox.Show("Hey, it's not my nose!!");
+            }
+        }
+
+
+    }
+}
